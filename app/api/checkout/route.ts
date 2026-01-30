d6c76687bfc5b1471e7c10550dc7e9055f17f51d
Petrica <petrica@example.com>
2026-01-30 12:02:32 +0000

    Use correct Stripe test price £1

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -7,17 +7,13 @@ export async function POST() {
   try {
    const session = await stripe.checkout.sessions.create({
   mode: "payment",
-
   line_items: [
     {
-      price: "price_1SvFTyJfM9TAIpM4rqFXY2U",
+      price: "price_1SvGG6JfM9TAIpMII3muaNPM",
       quantity: 1,
     },
   ],
-
-  success_url:
-    "https://big-discounts.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
-
+  success_url: "https://big-discounts.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
   cancel_url: "https://big-discounts.vercel.app/cart",
 });
 
