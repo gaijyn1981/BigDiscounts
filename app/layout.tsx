b01065b097cfa5a3f147e127a658ff2ed7c34a90
Petrica <petrica@example.com>
2026-02-25 21:35:24 +0000

    Add robots.txt, schema markup and page speed optimisations

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
           {children}
           <CookieBanner />
           <GoogleAnalytics />
+          <SchemaMarkup />
         </Providers>
       </body>
     </html>
